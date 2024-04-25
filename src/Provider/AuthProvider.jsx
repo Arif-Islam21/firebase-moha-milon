5435a0df25eb63e2eed4a01ee2f01716cd0a4e74
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-04-25 16:31:08 +0600

    authentication with context

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,13 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../Firebase/Firebase.config";
 
 export const AuthContext = createContext(null);
 
-const authInfo = { name: "nodi pahar" };
-
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = { user, createUser };
+
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
 
 export default AuthProvider;
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
